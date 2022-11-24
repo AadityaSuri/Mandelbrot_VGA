60e6ddb264a6f879f78b7a7ab3d5e0753e6a7036
Aaditya Suri <aadityasuri1@gmail.com>
2022-11-24 00:52:01 -0800

    something lol

--- mdbrot_main/mdbrot_top_level.sv
+++ mdbrot_main/mdbrot_top_level.sv
@@ -1,3 +1,7 @@
+`define start 2'd0
+`define scale_factor_calc 2'd1
+`define mandelbrot_draw 2'd2
+
 module mdbrot_top_level_single_nozoom(input logic CLOCK_50, input logic [3:0] KEY,
              input logic [9:0] SW, output logic [9:0] LEDR,
              output logic [6:0] HEX0, output logic [6:0] HEX1, output logic [6:0] HEX2,
@@ -8,7 +12,7 @@ module mdbrot_top_level_single_nozoom(input logic CLOCK_50, input logic [3:0] KE
              output logic [2:0] VGA_COLOUR, output logic VGA_PLOT);
 
     // instantiate and connect the VGA adapter and your module
-    logic start, done, vga_plot, VGA_BLANK, VGA_SYNC;
+    logic start, start_mdbrot, done, done_Xscale, done_Yscale, vga_plot, VGA_BLANK, VGA_SYNC;
     logic [2:0] colour;
     assign LEDR[9] = done;
     logic [7:0] vga_x;
@@ -16,11 +20,24 @@ module mdbrot_top_level_single_nozoom(input logic CLOCK_50, input logic [3:0] KE
     logic [2:0] vga_colour;
     assign start = KEY[3];
     logic [12:0] max_iter = 13'd500;
-    escape_time_mdbrot mandelbrot(CLOCK_50, KEY[3], start, max_iter, vga_x, vga_y, vga_colour, vga_plot);
+
+    logic signed [31:0] xmin, xmax, ymin, ymax, Xscale, Yscale;
+    assign xmin = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+    assign xmax = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+    assign ymin = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+    assign ymax = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+    assign Xscale = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+    assign Yscale = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+
+    escape_time_mdbrot mandelbrot(CLOCK_50, KEY[3], start_mdbrot, 
+                                  max_iter, xmin, xmax, ymin, ymax, Xscale, Yscale
+    max_iter, vga_x, vga_y, vga_colour, vga_plot);
     vga_adapter#(.RESOLUTION("160x120")) vga_u0(.resetn(KEY[3]), .clock(CLOCK_50), .colour(vga_colour),
                                             .x(vga_x), .y(vga_y), .plot(vga_plot),
                                             .VGA_R(VGA_R), .VGA_G(VGA_G), .VGA_B(VGA_B),
                                             .VGA_HS(VGA_HS), .VGA_VS(VGA_VS), .VGA_BLANK(VGA_BLANK), .VGA_SYNC(VGA_SYNC), .VGA_CLK(VGA_CLK));
+
+    
 
     // logic [31:0] zoom = 
     always_comb begin
--- mdbrot_main/qmult.v
+++ mdbrot_main/qmult.v
@@ -26,8 +26,8 @@ module qmult #(
 	(
 	 input			[N-1:0]	i_multiplicand,
 	 input			[N-1:0]	i_multiplier,
-	 output			[N-1:0]	o_result,
-	 output	reg				ovr
+	 output			[N-1:0]	o_result
+	//  output	reg				ovr
 	 );
 	 
 	 //	The underlying assumption, here, is that both fixed-point values are of the same length (N,Q)
@@ -47,7 +47,7 @@ module qmult #(
 	always @(i_multiplicand, i_multiplier)	begin						//	Do the multiply any time the inputs change
 		r_result <= i_multiplicand[N-2:0] * i_multiplier[N-2:0];	//	Removing the sign bits from the multiply - that 
 																					//		would introduce *big* errors	
-		ovr <= 1'b0;															//	reset overflow flag to zero
+		// ovr <= 1'b0;															//	reset overflow flag to zero
 		end
 	
 		//	This always block will throw a warning, as it uses a & b, but only acts on changes in result...
@@ -55,8 +55,8 @@ module qmult #(
 		r_RetVal[N-1] <= i_multiplicand[N-1] ^ i_multiplier[N-1];	//		which is the XOR of the input sign bits...  (you do the truth table...)
 		r_RetVal[N-2:0] <= r_result[N-2+Q:Q];								//	And we also need to push the proper N bits of result up to 
 																						//		the calling entity...
-		if (r_result[2*N-2:N-1+Q] > 0)										// And finally, we need to check for an overflow
-			ovr <= 1'b1;
+		// if (r_result[2*N-2:N-1+Q] > 0)										// And finally, we need to check for an overflow
+		// 	ovr <= 1'b1;
 		end
 
 endmodule
